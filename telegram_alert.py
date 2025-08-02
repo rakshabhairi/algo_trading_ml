857075590e5fefd6a97713e23688ea4069e365c8
rakshabhairi <bhairiraksha0@gmail.com>
2025-08-02 23:16:52 +0530

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_telegram_message(message):
-    token = "8244029092:AAFzkl2-EWbhmntkzkLW_gQh1c9EThxLqyU"  # replace with your bot token
-    chat_id = "7073228896"  # replace with your chat ID
+    token = "your_token"  # replace with your bot token
+    chat_id = "ur_chatid"  # replace with your chat ID
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     requests.post(url, data={'chat_id': chat_id, 'text': message})
